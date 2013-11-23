852fbe8d404d94b1784df5b4e1d3f45cc8e42690
Ian Connor <iconnor@projectlounge.com>
2013-11-24 07:59:41 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geoelevations::Application.config.secret_key_base = '99b1990c3747552e48a1ef1ae63a771a35d480339aa99bec42e480b658ad359548a9a0d511ee8c437e924f626393af8d22867f04547a1aac80446676d4c98039'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geoelevations::Application.config.session_store :cookie_store, key: '_geoelevations_session'
